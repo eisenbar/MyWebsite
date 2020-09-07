45083643148ea01cfe7ef958fb2ec12f19a067c5
eisenbar <ryan.eisenbarth@gmail.com>
2020-09-07 18:02:02 -0500

    added routing and static data fetching

--- mywebapp/pages/index.js
+++ mywebapp/pages/index.js
@@ -1,20 +1,65 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData} from "../lib/posts";
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home() {
+/*
+Use this for server side rendering on compile time
+Use to grab external data
+ */
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+/*
+Use this for server side rendering on runtime
+Used to pre-render data that needs to be fetched at request time
+ */
+/*export async function getServerSideProps(context) {
+    return {
+        props: {
+            // props for your component
+        }
+    }
+}*/
+
+export default function Home({allPostsData}) {
   return (
       <Layout home>
-        <Head>
+          <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
+          <section className={utilStyles.headingMd}>
           <p>Hello, I am a software developer</p>
           <p>
             (This is a sample website I have built with the help of {' '}
             <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
           </p>
         </section>
+          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <ul className={utilStyles.list}>
+                  {allPostsData.map(({ id, date, title}) => (
+                      <li className={utilStyles.listItem} key={id}>
+                          <Link href="/posts/[id]" as={`/posts/${id}`}>
+                              <a>{title}</a>
+                          </Link>
+                          <br />
+                          <small className={utilStyles.lightText}>
+                              <Date dateString={date} />
+                          </small>
+                      </li>
+                  ))}
+              </ul>
+          </section>
       </Layout>
   )
 }
