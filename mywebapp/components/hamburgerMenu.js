27d05938089cd3fb68b4862c7182e9cff50b0e15
eisenbar <ryan.eisenbarth@gmail.com>
2020-09-12 12:35:58 -0500

    added hamburger button and functionality

---
+++ mywebapp/components/hamburgerMenu.js
@@ -0,0 +1,40 @@
+import React from "react";
+import  hamStyles from "../styles/hamburger.module.css";
+import {bubble as Menu} from 'react-burger-menu';
+
+const menuItem = hamStyles.menuItem;
+
+export default function Hamburger({props}) {
+
+    return(
+     <Menu {...props}>
+         <a id="home" className={menuItem} href="/">Home</a>
+         <a id="about" className={menuItem} href="/aboutMe">About Me</a>
+         <a id="skills" className={menuItem} href="/skills">Skills</a>
+         <a id="experience" className={menuItem} href="/experience">Experience</a>
+
+     </Menu>
+    );
+}
+
+/*
+class Example extends React.Component {
+ showSettings (event) {
+   event.preventDefault();
+   .
+   .
+   .
+ }
+ render () {
+   // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+   return (
+     <Menu>
+       <a id="home" className="menu-item" href="/">Home</a>
+       <a id="about" className="menu-item" href="/about">About</a>
+       <a id="contact" className="menu-item" href="/contact">Contact</a>
+       <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+     </Menu>
+   );
+ }
+}
+ */
--- mywebapp/components/header.js
+++ mywebapp/components/header.js
@@ -1,10 +1,11 @@
-import style from "../styles/header.module.css";
+import headerStyle from "../styles/header.module.css";
 import React from "react";
+import Hamburger from './hamburgerMenu';
 
 export default function Header() {
     return (
-        <div className={`${style.bg} ${style.header}`}>
-            I am the header
+        <div className={`${headerStyle.bg} ${headerStyle.header}`}>
+            <Hamburger pageWrapId={"page-wrap"} outerContainerId={"Header"}/>
         </div>
     );
 }
