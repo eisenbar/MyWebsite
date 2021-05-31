9a81c66ed8515ff83ebc721431aaf9c62def5d8c
eisenbar <ryan.eisenbarth@gmail.com>
2021-05-30 23:10:33 -0500

    BIRDS!!!!

---
+++ mywebapp/components/birds.js
@@ -0,0 +1,28 @@
+import style from "../styles/bird.module.css";
+import React from "react";
+
+
+export default function Birds({home}) {
+    return (
+        <div className={style.container}>
+            <div className={`${style.birdContainer} ${style.birdContainerOne}`}>
+                <div className={`${style.bird} ${style.birdOne}`}/>
+            </div>
+
+            <div className={`${style.birdContainer} ${style.birdContainerTwo}`}>
+                <div className={`${style.bird} ${style.birdTwo}`}/>
+            </div>
+
+            <div className={`${style.birdContainer} ${style.birdContainerTwo}`}>
+                <div className={`${style.bird1} ${style.birdTwo}`}/>
+            </div>
+            <div className={`${style.birdContainer} ${style.birdContainerThree}`}>
+                <div className={`${style.bird1} ${style.birdThree}`}/>
+            </div>
+            <div className={`${style.birdContainer} ${style.birdContainerFour}`}>
+                <div className={`${style.bird1} ${style.birdFour}`}/>
+            </div>
+
+        </div>
+    );
+}
